e9a6d89b74f38f5207c3cbe5a24a8e45fd4418cd
Biel-Slow <134522836+Biel-Slow@users.noreply.github.com>
2023-10-25 11:40:27 -0300

    som

--- main.js
+++ main.js
@@ -4,3 +4,44 @@ function tocaSomPom(){
 }
 document.querySelector('.tecla_pom').onclick = tocaSomPom;
 
+function tocaSomClap(){
+    document.querySelector('#som_tecla_clap')
+}
+document.querySelector('.tecla_clap').onclick = tocaSomClap;
+
+function tocaSomTim(){
+    document.querySelector('#som_tecla_Tim')
+}
+document.querySelector('.tecla_tim').onclick = tocaSomTim;
+
+function tocaSomPuff(){
+    document.querySelector('#som_tecla_Puff')
+}
+document.querySelector('.tecla_Puff').onclick = tocaSomPuff;
+
+function tocaSomSplash(){
+    document.querySelector('#som_tecla_Splash')
+}
+document.querySelector('.tecla_splash').onclick = tocaSomSplash;
+
+function tocaSomToim(){
+    document.querySelector('#som_tecla_Toim')
+}
+document.querySelector('.tecla_Toim').onclick = tocaSomToim;
+
+function tocaSomPsh(){
+    document.querySelector('#som_tecla_psh')
+}
+document.querySelector('.tecla_psh').onclick = tocaSomPsh;
+
+function tocaSomTic(){
+    document.querySelector('#som_tecla_Tic')
+}
+document.querySelector('.tecla_Tic').onclick = tocaSomTic;
+
+function tocaSomTom(){
+    document.querySelector('#som_tecla_Tom')
+}
+document.querySelector('.tecla_Tom').onclick = tocaSomTom;
+
+
