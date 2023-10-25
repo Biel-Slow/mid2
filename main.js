b8c6db75cf1b06f6b8c098dd39bed5ecfd4be65c
Biel-Slow <134522836+Biel-Slow@users.noreply.github.com>
2023-10-25 09:09:43 -0300

    comit

--- main.js
+++ main.js
@@ -3,3 +3,7 @@ function tocaSomPom(){
     document.querySelector('#som_tecla_pom')
 }
 document.querySelector('.tecla_pom').onclick = tocaSomPom;
+document.querySelector('.tecla_pom').onclick = tocaSomPom;
+document.querySelector('.tecla_pom').onclick = tocaSomPom;
+document.querySelector('.tecla_pom').onclick = tocaSomPom;
+document.querySelector('.tecla_pom').onclick = tocaSomPom;
